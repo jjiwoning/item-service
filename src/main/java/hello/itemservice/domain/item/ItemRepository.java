5e4ef486e25945508caf48ccfa3bc4613e0847ea
JJiWoNing <jjiwoning12@gmail.com>
2022-06-29 02:47:23 +0900

    6/29_Study

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,41 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>(); // static -> 하나만 생성이 목표
+    // 멀티 스레드 환경에서는 HashMap을 쓰면 안된다. -> 싱글톤 객체에 여러 사람이 동시에 접근을 하기 때문
+    private static long sequence = 0L;
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Item> findAll(){
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam){
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore(){
+        store.clear();
+    }
+}
